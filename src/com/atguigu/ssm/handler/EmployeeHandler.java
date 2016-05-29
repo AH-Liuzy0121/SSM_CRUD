4299bdd3fa6c36f055006dec3683a8cccd292be0
liuzy <liuzy@192.168.127.1>
2016-05-29 16:26:53 +0800

    第二次提交

--- src/com/atguigu/ssm/handler/EmployeeHandler.java
+++ src/com/atguigu/ssm/handler/EmployeeHandler.java
@@ -27,6 +27,7 @@ public class EmployeeHandler {
 	private DepartmentService departmentService;
 
 	@RequestMapping("/emp/list")
+	//获取所有的员工信息
 	public String getEmpList(Map<String, Object> map) {
 
 		List<Employee> empList = employeeService.getEmpList();
@@ -37,7 +38,9 @@ public class EmployeeHandler {
 	}
 
 	@RequestMapping("/emp/addUI")
+	//进入添加员工的页面
 	public String addUI(Map<String, Object> map) {
+		//为了给员工划分部门，所以需要公司的所有部门
 		List<Department> deptList = departmentService.getDeptList();
 		map.put("deptList", deptList);
 
@@ -47,6 +50,7 @@ public class EmployeeHandler {
 	}
 
 	@RequestMapping(value = "/emp", method = RequestMethod.POST)
+	//员工添加
 	public String saveEmp(Employee employee) {
 
 		employeeService.saveEmp(employee);
@@ -56,14 +60,17 @@ public class EmployeeHandler {
 
 	@ResponseBody
 	@RequestMapping(value = "/emp/{id}", method = RequestMethod.DELETE)
+	//删除员工，加上@ResponseBody注解，不会再返回值上添加前后缀，使用Ajax在页面进行删除
 	public String removeEmp(@PathVariable("id") Integer id) {
 
+		//从数据库中删除
 		employeeService.removeEmp(id);
 
 		return "1";
 	}
 
 	@RequestMapping(value = "/emp/{id}", method = RequestMethod.GET)
+	//进入编辑页面，根据id获取员工信息
 	public String editUI(@PathVariable("id") Integer id, Map<String, Object> map) {
 
 		Employee employee = employeeService.getEmpById(id);
@@ -76,15 +83,19 @@ public class EmployeeHandler {
 	}
 
 	@ModelAttribute
+	//添加@ModelAttribute注解，在更新前会调用该方法获取Employee对象
 	public Employee getEmployee(
 			@RequestParam(value = "id", required = false) Integer id) {
 
 		Employee employee = employeeService.getEmpById(id);
 
+		//判断id是否存在，若不存在就是添加，返回空的Employee对象，若id存在就是更新
+		//根据指定的id获取员工信息
 		return (id == null) ? new Employee() : employee;
 	}
 	
 	@RequestMapping(value="/emp",method=RequestMethod.PUT)
+	//员工更新
 	public String updateEmp(Employee employee){
 		
 		employeeService.updateEmp(employee);
